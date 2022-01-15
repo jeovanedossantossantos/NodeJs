811ceb946d08f43272c29a7b7d9d44a7e9360260
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-01-15 14:00:28 -0300

    Operção código limpo(tirando códigos testes)

--- 06_aula_Projeto_final/blogApp/routes/admin.js
+++ 06_aula_Projeto_final/blogApp/routes/admin.js
@@ -9,97 +9,85 @@ router.get("/", (req, res) => {
 })
 
 router.get("/posts", (req, res) => {
-    // return res.json("Será mesmo?")
-    res.send("Pgina ok")
-})
-
+        res.send("Pgina ok")
+    })
+    // Rota de cadastro
 router.get("/categorias", (req, res) => {
-    // return res.json("Pagina principal da categorias")
-    // res.render("admin/categorias")
 
-    // Categorias.find().then((categorias) => {
-    //     res.render("admin/categorias", { categorias: categorias })
-    // }).catch((err) => {
-    //     res.flash("erros_msg", "Houve um essor na listgem ")
-    //     res.redirect("/admin")
+        Categorias.find().sort({ date: "desc" }).then(categorias => {
+            res.render('admin/categorias', {
+                categorias: categorias.map(categorias => categorias.toJSON())
+            })
+        }).catch((err) => {
+            res.flash("erros_msg", "Houve um essor na listgem ")
+            res.redirect("/admin")
 
-    //     // res.send("Pgina ok")
-    // })
-    Categorias.find().sort({ date: "desc" }).then(categorias => {
-        res.render('admin/categorias', {
-            categorias: categorias.map(categorias => categorias.toJSON())
+
         })
-    }).catch((err) => {
-        res.flash("erros_msg", "Houve um essor na listgem ")
-        res.redirect("/admin")
 
-        // res.send("Pgina ok")
-    })
+        router.get("/categorias/add", (req, res) => {
+            res.render("admin/addcategorias")
+        })
 
-    router.get("/categorias/add", (req, res) => {
-        // return res.json("Pagina principal da categorias")
-        res.render("admin/addcategorias")
-    })
+        router.post("/categorias/nova", (req, res) => {
 
-    router.post("/categorias/nova", (req, res) => {
+            var erros = []
 
-        var erros = []
-
-        if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
-            erros.push({
-                texto: "Nome inválido"
-            })
-        }
-        if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
-            erros.push({
-                texto: "Slug inválido"
-            })
-        }
-        if (req.body.nome.length < 2) {
-            erros.push({
-                texto: "Nome muito pequeno"
-            })
-        }
-
-        if (erros.length > 0) {
-            res.render("admin/addcategorias", {
-                erros: erros
-            })
-        } else {
-            const novaCategora = {
-                nome: req.body.nome,
-                slug: req.body.slug
+            if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+                erros.push({
+                    texto: "Nome inválido"
+                })
             }
-            new Categorias(novaCategora).save().then(() => {
-                req.flash("success_msg", "Salvo com sucesso")
-                res.redirect("/admin/categorias")
-            }).catch((err) => {
-                req.flash("error_msg", "Erro ao salvar")
-                res.redirect("/admin")
-            })
-        }
+            if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+                erros.push({
+                    texto: "Slug inválido"
+                })
+            }
+            if (req.body.nome.length < 2) {
+                erros.push({
+                    texto: "Nome muito pequeno"
+                })
+            }
 
-    })
-
-})
-
-router.get("/categorias/edit/:id", (req, res) => {
-    // res.send("Pagina de edição de categoras")
-    Categorias.findOne({ _id: req.params.id }).then((categoria) => {
-        // res.render("admin/editar", { categoria: categoria })
-        res.render('admin/editar', {
-            categoria: categoria.toJSON()
+            if (erros.length > 0) {
+                res.render("admin/addcategorias", {
+                    erros: erros
+                })
+            } else {
+                const novaCategora = {
+                    nome: req.body.nome,
+                    slug: req.body.slug
+                }
+                new Categorias(novaCategora).save().then(() => {
+                    req.flash("success_msg", "Salvo com sucesso")
+                    res.redirect("/admin/categorias")
+                }).catch((err) => {
+                    req.flash("error_msg", "Erro ao salvar")
+                    res.redirect("/admin")
+                })
+            }
 
         })
-        console.log(categoria)
-    }).catch((err) => {
-        req.flash("erro_msg", "Esta categoria não existe")
-        res.redirect("/admin")
+
     })
+    // Rota de verificação de categoria
+router.get("/categorias/edit/:id", (req, res) => {
+
+        Categorias.findOne({ _id: req.params.id }).then((categoria) => {
+
+            res.render('admin/editar', {
+                categoria: categoria.toJSON()
+
+            })
+            console.log(categoria)
+        }).catch((err) => {
+            req.flash("erro_msg", "Esta categoria não existe")
+            res.redirect("/admin")
+        })
 
 
-})
-
+    })
+    // Rota de edição
 router.post("/categorias/edit/:id", async(req, res) => {
 
     try {
@@ -111,43 +99,9 @@ router.post("/categorias/edit/:id", async(req, res) => {
         res.redirect("/admin/categorias")
     } catch (err) {
         console.log("Erro " + err)
+        res.redirect("/admin/categorias")
     }
-    // const r = Categorias.updateOne({ _id: req.params.id }, req.body, (err) => {
-    //     if (err) {
-    //         console.log("erro " + err)
-    //     } else {
-    //         console.log("Atualizado")
-    //         res.redirect("/admin/categorias")
-    //     }
 
-    // })
 })
-
-// router.post("/categorias/edit", (req, res) => {
-//     Categorias.find({ _id: req.body.id }).then((categoria) => {
-//         categoria = req.body
-//             // categoria.slug = req.body.slug
-//         console.log(categoria)
-
-
-
-//         categoria.save().then(() => {
-//             req.flash("success_msg", "Sucess")
-//             res.redirect("/admin/categorias")
-//             console.log("salvo" + req.body.nome)
-//         }).catch((err) => {
-//             console.log("Erro " + err)
-//             req.flash("error_msg", "Erro")
-//             res.redirect("/admin/categorias")
-//         })
-//     }).catch((err) => {
-//         console.log("Erro 2 " + err)
-//             // req.flash("error_msg", "Erro")
-//             // res.redirect("/admin/categorias")
-//     })
-// })
-
-
-
 
 module.exports = router
